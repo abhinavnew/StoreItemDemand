22560a25ab64c4032d0ad35dbb130934aec401f8
AbhinavB <abhinavnew@gmail.com>
2019-12-20 14:24:45 +0530

    Timeseries feature creation

--- StoreItemdemand_initial.R
+++ StoreItemdemand_initial.R
@@ -20,6 +20,7 @@ library(e1071)
 library(glmnet)
 library(pROC)
 library(funModeling)
+library(lubridate)
 
 
 ##Notations off and clear all objects
@@ -35,6 +36,10 @@ zerovari <- function(dat) {
   unlist(want)
 }
 
+first_day_of_month_wday <- function(dt) {
+  day(dt) <- 1
+  wday(dt)
+}
 
 lift <- function(depvar, predcol, groups=10) {
   if(!require(dplyr)){
@@ -55,9 +60,33 @@ lift <- function(depvar, predcol, groups=10) {
 
 storedata_orig=fread("E:\\AbhinavB\\Kaggle\\StoreItemDemandPrediction\\train.csv",
                    data.table = FALSE,stringsAsFactors = TRUE)
+
                    
 
 storedata=storedata_orig
 dim(storedata)
+
+##date type feature creation 
+
+storedata$date2=ymd(storedata$date)
+storedata$YEAR=lubridate::year(storedata$date2)
+storedata$MNTH_NM=lubridate::month(storedata$date2,label=TRUE)
+##Day of week month year 
+storedata$DayOfWeek=lubridate::wday(storedata$date2)
+storedata$DayOfWeek_Name=lubridate::wday(storedata$date2,label=TRUE)
+storedata$DayOfMonth=lubridate::mday(storedata$date2)
+storedata$DayOfYear=lubridate::yday(storedata$date2)
+##Week of year & month
+storedata$WeekOfYr=lubridate::week(storedata$date2)
+storedata$WeekOfMnth=ceiling((day(storedata$date2)+first_day_of_month_wday(storedata$date2)-1)/7)
+
+
+##volume of items sold related feature creation 
+
+
+
+
+
+
 
 
